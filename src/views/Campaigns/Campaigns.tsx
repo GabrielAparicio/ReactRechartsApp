76b6174e63b509beac19efdfe4412eb1c59431ae
GabrielAparicio <sgaparicio22@gmail.com>
2021-06-16 23:30:39 -0500

    Update campaigns view

--- src/views/Campaigns/Campaigns.tsx
+++ src/views/Campaigns/Campaigns.tsx
@@ -1,5 +1,64 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import LoadingView from '../LoadingView';
+import {
+  selectCampaignIds,
+  selectStatus,
+  fetchCampaigns,
+} from '../../features/campaigns/campaignsSlice';
+import { useAppSelector, useAppDispatch } from '../../redux/storeHooks';
+import AllCampaigns from '../../features/campaigns/components/AllCampaigns';
+import CreateCampaignForm from '../../features/campaigns/components/CreateCampaignForm';
 
-const Campaigns: React.FC = () => <div>Campaigns</div>;
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      height: '100vh',
+    },
+    firstRow: {
+      height: '20%',
+      margin: 'auto',
+    },
+    secondRow: {
+      height: '80%',
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+  })
+);
+
+const Campaigns = () => {
+  const classes = useStyles();
+  const dispatch = useAppDispatch();
+  const campaignIds = useAppSelector(selectCampaignIds);
+  const status = useAppSelector(selectStatus);
+
+  useEffect(() => {
+    dispatch(fetchCampaigns());
+  }, []);
+
+  if (status === 'loading') return <LoadingView />;
+
+  if (status === 'failed') return <div>Failed!</div>;
+
+  return (
+    <Grid container direction="column" className={classes.root}>
+      {status === 'idle' && campaignIds.length > 0 ? (
+        <>
+          <Grid item className={classes.firstRow}>
+            <CreateCampaignForm />
+          </Grid>
+          <Grid item className={classes.secondRow}>
+            <AllCampaigns campaignIds={campaignIds} />
+          </Grid>
+        </>
+      ) : (
+        <div>Empty data</div>
+      )}
+    </Grid>
+  );
+};
 
 export default Campaigns;
