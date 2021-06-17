1b98617469be8d59f4f5be8ec484ced0ec0fef9b
GabrielAparicio <sgaparicio22@gmail.com>
2021-06-16 23:17:08 -0500

    Fix eslint conflict

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,7 +1,7 @@
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import { BASE_URL, OVERVIEW_ENDPOINT } from '../services/axiosService/api';
-import { overviewData } from './mockOverviewData';
+import overviewData from './mockOverviewData';
 
 const server = setupServer(
   rest.get(`${BASE_URL}${OVERVIEW_ENDPOINT}`, (req, res, ctx) => {
