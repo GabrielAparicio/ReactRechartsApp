0b94170302573a9f8d0e99b834f2aff0d953e07a
GabrielAparicio <sgaparicio22@gmail.com>
2021-06-16 21:12:29 -0500

    Fix format

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      Front end task
-    </div>
-  );
+  return <div className="App">Front end task</div>;
 };
 
 export default App;
